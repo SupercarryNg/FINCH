36838b1be11ceb85c3c29d8efc8234141e9ba5e6
WU Junyan <85872793+SupercarryNg@users.noreply.github.com>
2022-07-11 23:28:53 +0800

    Add files via upload
    add visualization plot titles
    add groud truth plot
    add mnist dataset
    add Normalization

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from finch import FINCH
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import preprocessing
 
 import umap
 
 import os
+from time import time
 from collections import Counter
 
 
@@ -20,22 +22,37 @@ class Clustering:
 
     def load_data(self):
 
-        print('Loading the {} Dataset'.format(self.dataset))
+        print('Loading the {} Dataset...'.format(self.dataset))
+        minmax_scale = preprocessing.MinMaxScaler()  # Normalization method
 
         if self.dataset == 'wine':
             df = pd.read_csv('data/wine.txt', sep=",", index_col=None)
             lbs = df.cls  # lbs means the Ground Truth Labels -> shape of (N, 1)
             feature = df.iloc[:, 1:]  # feature that we use for clustering later -> shape of (N, num of feature)
             print('There are {} clusters in the true dataset'.format(len(lbs.unique())))
+
+            feature = minmax_scale.fit_transform(feature)
             self.data = feature
             self.lbs = lbs
 
         elif self.dataset == 'mnist':
-            pass
+            df = pd.read_csv('data/mnist.csv', index_col=None)
+            lbs = df.label
+            feature = df.iloc[:, 1:]
+            print('There are {} clusters in the true dataset'.format(len(lbs.unique())))
+
+            feature = minmax_scale.fit_transform(feature)
+            self.data = feature
+            self.lbs = lbs
 
     def fit(self):
         if self.method == 'FINCH':
+            print('Fitting {} dataset with {} method...'.format(self.dataset, self.method))
+            start = time()
             c, num_clust, req_c = FINCH(self.data)  # c: (N, P) pred labels for each sample
+            end = time()
+            runTime = end - start
+            print("RunTime：", runTime)
             self.res = c
 
     @staticmethod
@@ -50,20 +67,27 @@ class Clustering:
             os.makedirs(path)
 
         # we wanna plot each result of clustering hierarchy
-
+        embedding = self.reduce(self.data)
+        n = len(embedding)
         for i in range(self.res.shape[1]):
             pred_lbs = self.res[:, i]
-            embedding = self.reduce(self.data)
             counter = Counter(pred_lbs)  # {0: 137, 1: 15, 2: 16, ...}
-            n = len(embedding)
 
             # Use a for loop to plot scatter of different clusters
             for j in range(len(np.unique(pred_lbs))):
                 plt.plot(embedding[:, 0][pred_lbs == j], embedding[:, 1][pred_lbs == j], '.', ms=3, alpha=0.5,
                          label='cluster {}: {:.2f}%'.format(j, counter[j] / n * 100))
-
+            plt.title('{} method ({} cluster)'.format(self.method, len(np.unique(pred_lbs))))
             plt.savefig(path + 'Partition_{}_'.format(i) + 'cluster_plot')
             plt.show()
+
+        counter_gt = Counter(self.lbs)
+        for k in range(len(np.unique(self.lbs))):
+            plt.plot(embedding[:, 0][self.lbs == k], embedding[:, 1][self.lbs == k], '.', ms=3, alpha=0.5,
+                     label='cluster {}: {:.2f}%'.format(k, counter_gt[k] / n * 100))
+
+        plt.savefig(path + 'GroundTruth_' + 'cluster_plot')
+        plt.show()
 
     def evaluation(self):
         # we need to maximize the matched accuracy
@@ -71,11 +95,10 @@ class Clustering:
 
 
 if __name__ == '__main__':
-    model = Clustering()
+    model = Clustering(dataset='mnist')
     model.load_data()
     model.fit()
     model.visualization()
-    print(model.res)
 
 
 
